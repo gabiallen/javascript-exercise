454a955e47901a8934f107ba51bf1f0469ffb50f
gabiallen <gabiallen@users.noreply.github.com>
2022-04-05 23:30:07 +0000

    deploy: 5739f22cee14511fa034a5e8cbfd741921ad8dd9

--- js/main.js
+++ js/main.js
@@ -1,1 +1,15 @@
+var firstName = "Gabi";
+var age = 20;
+var favoriteFood = "Brussel Sprouts";
 
+console.log(
+  `My name is ${firstName}. I am ${age} years old and I love to eat ${favoriteFood}.`
+);
+
+var num = 2;
+let result = num + num;
+console.log(result);
+
+var firstName = "Gabi";
+var lastName = "Allen";
+console.log(`${firstName} ${lastName}`);
